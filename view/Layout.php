777c2882bccf7eec827b39117195deb919408b4f
Miguel <miguela.aragar@educa.jcyl.es>
2020-01-16 08:54:27 +0100

    empezando a programar

--- view/Layout.php
+++ view/Layout.php
@@ -45,7 +45,7 @@
         
          <footer class="page-footer font-small blue load-hidden">
             <div class="footer-copyright text-center py-3"> <a href="../../../index.php">© 2019 Copyright: Ismael Heras Salvador</a> 
-                <a href="http://daw-usgit.sauces.local/heras/ProyectoLoginLogoff/tree/master"><img  src="WEBBROOT/img/gitLab.png" alt=GitLab""></a>
+                <a href="http://daw-usgit.sauces.local/heras/LoginLogoffMulticapaPOO/tree/master"><img  src="WEBBROOT/img/gitLab.png" alt=GitLab""></a>
                 <a href="https://github.com/ismaelom83/LoginLogoffMulticapaPOO"><img  src="WEBBROOT/img/gitHub.png" alt="GitHub"></a>
                 <a href="../DWES.php">Salir De La Aplicacion</a> 
             </div>
